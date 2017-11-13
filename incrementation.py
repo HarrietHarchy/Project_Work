e40ba3a7101bc4a3c43e41029ebba147d2589d32
HarrietHarchy <harriet@aumaharriet@gmail.comcdcd>
2017-11-13 08:19:52 -0500

    Application of For and While loops

---
+++ incrementation.py
@@ -0,0 +1,16 @@
+#I want numbers from 0 to 100 in lengths of 2. Also called even numbers.
+for i in range(0,101,2):
+	print(i)
+
+#I want odd numbers from 1 to 100.
+for i in range(1,101,2):
+ 	print(i)
+
+#I want to find all the prime numbers from 0 to 10.
+counter = 3
+while counter < 10:
+ 	print(counter)
+
+ 	counter = counter + 1
+
+
